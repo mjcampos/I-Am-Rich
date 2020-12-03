dacb49b8dfb77c0dae9f52a3fe47f8dbcdaa1be8
Michael Campos <camposmj89@gmail.com>
2020-12-03 11:14:37 -0800

    Initial setup

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Michael Campos on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
